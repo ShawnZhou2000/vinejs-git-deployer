caf6b3fb27f86b4bd8ebe0929b0d597c664285aa
ShawnZhou2000 <shawnzhou101@163.com>
2022-04-25 22:49:33 +0800

    feat: 写不完了，下班了

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const util = require('util');
 const exists = util.promisify(fs.exists);
-const cmd = require('node-cmd');
+const _cmd = require('node-cmd');
+const cmd = util.promisify(_cmd.run);
 const _yaml = require('js-yaml');
 const log = require('./utils/colorLog.js');
 const path = require('path');
@@ -19,13 +20,8 @@ const getYaml = function(dir) {
   });
 }
 
-// TODO: 强耦合，后期考虑抽离
-const deployerMap = [
-  'git',
-  'tencent cloudbase',
-  'aliyun server',
-  'aliyun oss'
-];
+let ymlConfig = {};
+const commonPath = '../dist';
 
 /*
 0. 校验vine.deployer.yml是否合法，如合法则解析
@@ -36,21 +32,24 @@ const deployerMap = [
 function publish() {
   getYaml('./vine.deployer.yml')
   .then(res => {
-    if (!deployerMap.includes(res.type)) {
+    if (res.type !== 'git') {
       log.error(`Deployer type invalid, please check your config.`);
       process.exit(1);
     }
-    if (res.type === 'git' && !(res.repo && res.branch && res.auth)) {
+    if (!(res.repo && res.branch && res.auth.user_name && res.auth.user_email)) {
       log.error('You should configure deployer settings in vine.deployer.yml first, see this example:');
       console.log(
         `        type: git
         repo: <your git repo url>
         branch: main
+        auth:
+          user_name: <your git user name>
+          user_email: <your git user email></your>
         extend_dir: [extend directory]
       `)
       process.exit(1);
     }
-    // TODO: 接入其他部署器时使用策略模式优化
+    ymlConfig = res;
     return res;
   })
   .catch(err => {
@@ -60,22 +59,55 @@ function publish() {
     process.exit(1);
   })
   .then(res => {
-    return Promise.all([exists(path.resolve( '../dist')), res]);
+    return exists(path.resolve( commonPath));
   })
   .then(res => {
-    console.log(res);
     // if 'dist' don't exist
-    if (!res[0]) {
+    if (!res) {
       log.error(`cannot find path 'dist', did you forget to build this project?`);
       log.error(`try run command 'vine build' first.`);
       process.exit(1);
     }
-    return Promise.all([exists(path.resolve('../dist', '.git')), res[1]]);
+    return exists(path.resolve(commonPath, '.git'));
   })
   .catch(err => {
     log.error('Oops, something wrong in deployer.');
     log.error(err);
     log.error(`Vine.js can't find 'vine.deployer.yml', please check your config.`);
+    process.exit(1);
+  })
+  .then(res => {
+    // if '.git' already exist
+    log.info('Setting up Git deployment...');
+    if (res) {
+      fs.rmdirSync(path.resolve(commonPath, '.git'));
+      log.info('Rebuild .git directory...');
+    }
+    return cmd('git init');
+  })
+  .then(() => {
+    return cmd(`git config user.name ${ymlConfig.auth.user_name}`);
+  })
+  .then(() => {
+    return cmd(`git config user.email ${ymlConfig.auth.user_email}`);
+  })
+  .then(() => {
+    return cmd(`git add .`);
+  })
+  .then(() => {
+    // TODO: 后面加上更新时间
+    return cmd(`git commit -m 'site updated'`);
+  })
+  .then(() => {
+    return cmd(`git push -u ${ymlConfig.repo} HEAD:${ymlConfig.branch} --force`);
+  })
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+    log.error('Oops, something wrong in deployer.');
+    // log.error(`Vine.js can't find 'vine.deployer.yml', please check your config.`);
     process.exit(1);
   })
 }
